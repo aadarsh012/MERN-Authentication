c307743dddb9f4c0d31cfc898c5cde122350af5d
aadarsh012 <aadarsh012@gmail.com>
2021-09-21 00:46:14 +0530

    Final Deployment

--- config/db.js
+++ config/db.js
@@ -1,11 +1,15 @@
 const mongoose = require("mongoose");
 
 const connectDB = async function () {
-  await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  });
-  console.log("MongoDb Connected");
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log("MongoDb Connected");
+  } catch (error) {
+    console.log("error", error);
+  }
 };
 
 module.exports = connectDB;
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require("dotenv").config({ path: "./config.env" });
 
+const path = require("path");
+
 const express = require("express");
 
 const connectDB = require("./config/db");
@@ -17,6 +19,18 @@ app.use("/api/private", require("./routes/private"));
 
 // Error Handler Middleware(Placed at the last)
 app.use(errorHandler);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Api Running");
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
