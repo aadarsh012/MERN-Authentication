d11b08fe80a41b86c4a4361ab458c2adaa9c1677
aadarsh012 <aadarsh012@gmail.com>
2021-09-22 19:52:25 +0530

    Modified

--- client/src/App.js
+++ client/src/App.js
@@ -5,27 +5,24 @@ import Login from "./Components/Login/Login";
 import Register from "./Components/Register/Register";
 import ForgotPassword from "./Components/ForgotPassword/ForgotPassword";
 import ResetPassword from "./Components/ResetPassword/ResetPassword";
-import Aux from "./HOC/auxilliary";
 
 function App() {
   return (
-    <Aux>
-      <div className="App">
-        <Switch>
-          <Route path="/passwordreset/:resetToken" component={ResetPassword} />
-          <Route path="/forgotpassword" component={ForgotPassword} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-          <PrivateRoute exact path="/" />
-        </Switch>
-      </div>
+    <div className="App">
+      <Switch>
+        <Route path="/passwordreset/:resetToken" component={ResetPassword} />
+        <Route path="/forgotpassword" component={ForgotPassword} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+        <PrivateRoute exact path="/" />
+      </Switch>
       <footer>
         <p>Created By AADARSH SHAW </p>
         <a href="https://github.com/aadarsh012" target="_blank">
           <img src="github-logo.png" />
         </a>
       </footer>
-    </Aux>
+    </div>
   );
 }
 
